b0d7fc320335807dd9c3dffb694a40a84ac938e7
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-04-13 16:47:56 +0800

    user router

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const http = require('http')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
+const router = require('./router')
+
 const app = express()
 
 // App Setup
 app.use(morgan('combined')) // used for logging
 app.use(bodyParser.json({ type: '*/*' })) // parse incoming request. parse as json(as defined)
-
+router(app)
 
 // Server Setup
 const port = process.env.PORT || 3090
